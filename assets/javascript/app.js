00377e1fa51a51c2c24dab90883b6fc338ef2d7b
salilbhise <Salil@Shekhs-MacBook-Pro.local>
2018-09-27 19:53:31 -0500

    begin js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+$(document).ready(function(){
+
 // Initialize Firebase
 var config = {
     apiKey: "AIzaSyCzFM_4aP6m-4lzU0EB0UsZw29YAwXm3XU",
@@ -7,6 +9,8 @@ var config = {
     storageBucket: "train-scheduler-bf22d.appspot.com",
     messagingSenderId: "52621676133"
   };
+
   firebase.initializeApp(config);
   
-  
+  // Create varibale to reference database
+  var database=firebase.database();
